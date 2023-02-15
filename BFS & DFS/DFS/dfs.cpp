1dd2895f3a62d49f88a389972e7fdfd334183dfe
Sheikh Roman <64395017+sheikhroman@users.noreply.github.com>
2023-02-15 16:31:49 +0600

    Add files via upload

---
+++ BFS & DFS/DFS/dfs.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+const int N = 1e5;
+
+int visited(N);
+
+vector<int> adj_list[N];
+
+void dfs(int node){
+    cout << node << endl;
+    visited[node] = 1;
+    
+    for(int adj_node: adj_list[node]){
+        if(visited[adj_node]==0){
+            dfs(adj_node);
+        }
+    }
+
+}
+int main(){
+    int nodes, edges;
+    cin >> nodes >> edges;
+    for (int i = 0; i < edges; i++){
+        int u, v;
+        cin >> u >> v;
+        adj_list[u].push_back(v);
+        adj_list[v].push_back(u);
+    }
+    int src = 0;
+    dfs(src);
+}
+/*
+6 6
+
+0 1
+1 2
+2 3
+3 5
+4 4
+5 5
+*/
---
+++ BFS & DFS/Recursion/recursion.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int fibonacci(int n){
+    //base case
+    if(n==0)return 0;
+    if(n==1)return 1;
+
+    //general case
+    int num1 = fibonacci(n - 1);
+    int num2 = fibonacci(n - 2);
+    return num1 + num2;
+    // return fibonacci(n - 1) + fibonacci(n - 2);   
+}
+int main(){
+    int n;
+    cin >> n;
+    cout << fibonacci(n) << endl;
+    return 0;
+}
+//
